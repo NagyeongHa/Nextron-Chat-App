81312b9dd6b791d9278e2e80fb520f99d6575781
NagyeongHa <hnk3176@naver.com>
2023-01-12 04:16:24 +0900

    feat : 유저 목록 불러오기

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -1,22 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Head from "next/head";
 import useOnAuthStateChange from "../hooks/useOnAuthStateChange";
 import Title from "../components/Title";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
+import Image from "next/image";
 
 function Home() {
   const onAuthState = useOnAuthStateChange();
   const { isLoggedin, userInfo } = onAuthState;
+  const [userList, setUserList] = useState([]);
 
-  console.log(isLoggedin, userInfo);
+  useEffect(() => {
+    const getUsers = async () => {
+      const getUserCollection = await getDocs(collection(db, "users"));
+      const users = getUserCollection.docs.map(doc => doc.data());
+      setUserList(users);
+    };
+    getUsers();
+  }, []);
+
+  console.log(userList);
 
   return (
     <>
       <Head>
         <title></title>
       </Head>
-      <div>
-        <Title title='유저 목록' />
-      </div>
+
+      <Title title='유저 목록' />
+      {userList.map(user => (
+        <div key={user.email}>
+          <Image
+            className='rounded-2xl'
+            src={user.profileimage}
+            width={50}
+            height={50}
+          ></Image>{" "}
+          {user.name}
+        </div>
+      ))}
     </>
   );
 }
